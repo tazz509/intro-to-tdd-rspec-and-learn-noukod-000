eebab5f06a96e41a7d7ca0acd8e627729bf9ed58
carly <carly@noukod.com>
2018-08-29 15:13:48 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -3,7 +3,7 @@ describe "current_age_for_birth_year method" do
     current_year = time.now.year
     birth_year = 1984
     answer = current_year - birth_year
-    age_of_person = current_age_for_birth_year(1999)
+    age_of_person = current_age_for_birth_year(1984)
 
     expect(age_of_person).to eq(19)
   end
