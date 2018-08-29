75a95af54c32e171b885067a2a17b8e2f1cc9d48
carly <carly@noukod.com>
2018-08-29 15:08:38 +0000

    automatically backed up by learn

---
+++ current_age_for_birth_year.rb
@@ -0,0 +1,7 @@
+describe "current_age_for_birth_year method" do
+  it "returns the age of a person based on the year of birth" do
+    age_of_person = current_age_for_birth_year(1999)
+ 
+    expect(age_of_person).to eq(19)
+  end
+end
